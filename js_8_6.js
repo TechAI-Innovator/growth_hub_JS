b7439555c7c6b6bab8251e2a1842d05e9d1fc050
Ayomide Oluwanifesimi Adu <aduayomide5@gmail.com>
2025-08-09 22:21:02 +0100

    2 changes

---
+++ js_8_6.js
@@ -0,0 +1,58 @@
+// const profile = {
+//     name:"Adu Ayomide",
+//     can_swim: true,
+//     laptop:{
+//         size: "small",
+//         color: "black",
+//         model: "macbook pro",
+//         year: 2022
+//     },
+//     hobbies:["ball", "singing", "dancing", "games"]
+// }
+
+
+// console.log(typeof profile.hobbies)
+
+
+// let a = 10, b=3;
+// console.log(a+b, a-b, a*b, a/b, a%b, a**b)
+
+// console.log(5=="5");
+// console.log(5==="5");
+
+// console.log("True"=="true");
+
+
+// user_age = 18;
+
+// if(user_age >= 18){
+//     console.log("You are eligible to vote");
+// }else{
+//     console.log("You are not eligible to vote");
+// }
+
+
+
+// user_age = 18;
+
+// result = user_age > 18 ? "You are eligible": "You are not eligible"
+// console.log(result)
+
+
+
+//  x = 5; x++;
+// console.log(x);
+//  z = x;
+// console.log(z);
+
+
+let num = 3;
+for (let i = 1; i<=12; i++) {
+    console.log(num, "x", i, "=", num*i);
+}
+
+let num1 = 10, count = 1;
+while (count<13) {
+    console.log(`${num1} X ${count} = ${num1*count}`);
+    count++;
+}
